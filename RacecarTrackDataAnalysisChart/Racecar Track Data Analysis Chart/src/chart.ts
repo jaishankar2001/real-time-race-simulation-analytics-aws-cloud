b0c6155bd575202387aed5fcac9807cc7667b13c
jaishankar2001 <92016645+jaishankar2001@users.noreply.github.com>
2024-06-25 10:00:00 -0300

    feat: input heatmap webpage

---
+++ RacecarTrackDataAnalysisChart/Racecar Track Data Analysis Chart/src/chart.ts
@@ -0,0 +1,146 @@
+/**
+ * Example showcasing Line Series feature for coloring line dynamically based on separate Value data set
+ */
+import speed from './demo-data-speed.json'
+import steering from './demo-data-steering.json'
+import throttle from './demo-data-throttle.json'
+
+ const lcjs = require("@arction/lcjs");
+ const {
+   lightningChart,
+   UIElementBuilders,
+   UIOrigins,
+   PalettedFill,
+   LUT,
+   ColorRGBA,
+   emptyLine,
+   AxisTickStrategies,
+   Themes,
+ } = lcjs;
+ 
+ const chart = lightningChart()
+   .ChartXY({
+     theme: Themes.darkGold,
+   })
+   .setTitle("Loading example data ...")
+   
+ 
+ const axisX = chart.getDefaultAxisX();
+ const axisY = chart.getDefaultAxisY();
+ chart
+   .getDefaultAxes()
+   .forEach((axis) =>
+     axis.setTickStrategy(AxisTickStrategies.Empty).setStrokeStyle(emptyLine)
+   );
+ 
+ const lineSeries = chart
+   .addLineSeries({ individualLookupValuesEnabled: true })
+   .setCursorSolveBasis("nearest")
+   .setCursorInterpolationEnabled(false)
+   .setStrokeStyle((stroke) => stroke.setThickness(10));
+ 
+ const labelStart = chart
+   .addUIElement(UIElementBuilders.TextBox, { x: axisX, y: axisY })
+   .setMouseInteractions(false)
+   .dispose();
+ 
+ const dataSources = [
+   {
+     name: "Speed",
+     file: speed,
+     lut: new LUT({
+       steps: [
+         { value: 0, color: ColorRGBA(255, 255, 255) },
+         { value: 25, color: ColorRGBA(255, 215, 0) },
+         { value: 50, color: ColorRGBA(255, 0, 0) },
+       ],
+       interpolate: true,
+       units: "km/h",
+     }),
+     format: (value) => `${value.toFixed(1)} km/h`,
+   },
+   {
+     name: "Steering",
+     file: steering,
+     lut: new LUT({
+       steps: [
+         { value: -100, color: ColorRGBA(255, 255, 0) },
+         { value: 0, color: ColorRGBA(255, 255, 255) },
+         { value: 100, color: ColorRGBA(0, 0, 255) },
+       ],
+       interpolate: true,
+       units: "%",
+     }),
+     format: (value) => `${value.toFixed(1)} %`,
+   },
+   {
+     name: "Throttle",
+     file: throttle,
+     lut: new LUT({
+       steps: [
+         { value: 0, color: ColorRGBA(255, 255, 255) },
+         { value: 50, color: ColorRGBA(255, 215, 0) },
+         { value: 100, color: ColorRGBA(255, 0, 0) },
+       ],
+       interpolate: true,
+       units: "%",
+     }),
+     format: (value) => `${value.toFixed(1)} %`,
+   },
+ ];
+ 
+ let legend;
+ const displayDataSource = async (sourceName) => {
+   // CLeanup previously displayed data source
+   if (legend) {
+     legend.dispose();
+     legend = undefined;
+   }
+ 
+   const dataSource = dataSources.find((item) => item.name === sourceName);
+   let data = dataSource.file;
+ 
+   lineSeries
+     .clear()
+     .setName(sourceName)
+     .add(data)
+     .setStrokeStyle((stroke) =>
+       stroke.setFillStyle(new PalettedFill({ lut: dataSource.lut }))
+     )
+     .setCursorResultTableFormatter((builder, _, __, ___, dataPoint) =>
+       builder
+         .addRow(sourceName)
+         .addRow(dataPoint.value && dataSource.format(dataPoint.value))
+     );
+ 
+   const start = data[0];
+   labelStart
+     .restore()
+     .setPosition(start)
+     .setOrigin(UIOrigins.CenterBottom)
+     .setMargin(10)
+     .setText("START");
+ 
+   chart.setTitle(`Racecar ${sourceName} progression during 1 lap`);
+ 
+   legend = chart.addLegendBox().add(chart);
+ };
+ displayDataSource("Speed");
+ 
+ // Embed HTML UI inside the chart for selecting between different data sources.
+ const uiDiv = document.createElement("div");
+ chart.engine.container.append(uiDiv);
+ uiDiv.style.position = "absolute";
+ uiDiv.style.top = `${chart.getTitleMarginTop()}px`;
+ uiDiv.style.right = `${chart.getPadding().right}px`;
+ const inputDataSource = document.createElement("select");
+ uiDiv.append(inputDataSource);
+ dataSources.forEach((dataSource) => {
+   const option = document.createElement("option");
+   inputDataSource.append(option);
+   option.innerHTML = dataSource.name;
+ });
+ inputDataSource.onchange = (e) => {
+   displayDataSource((e.target as HTMLInputElement).value);
+ };
+ 
